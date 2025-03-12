847afd79f54d13544fbaf00607db166e393cfc57
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-12 23:17:06 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// Code your solution in tf
+function distanceFromHqInBlocks(pickUpLocation) {
+  const headquarters = "57";
+  return Math.abs(pickUpLocation - headquarters);
+}
+
+console.log(distanceFromHqInBlocks(50));
+console.log(distanceFromHqInBlocks(63));
+
+function distanceFromHqInFeet(pickUpLocation){
+let blocks = distanceFromHqInBlocks(pickUpLocation);
+  return blocks * 264;
+}
+
+console.log(distanceFromHqInFeet(50));
+console.log(distanceFromHqInFeet(67));
+
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264;
+}
+
+console.log(distanceTravelledInFeet(34, 38)); 
+console.log(distanceTravelledInFeet(50, 60)); 
+console.log(distanceTravelledInFeet(60, 50)); 
