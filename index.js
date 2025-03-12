0863f9c86571fe22eabb75cb0d6a4c35ad36e63d
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-13 00:24:40 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Code your solution in tf
 function distanceFromHqInBlocks(pickUpLocation) {
-  const headquarters = "57";
+  const headquarters = "42";
   return Math.abs(pickUpLocation - headquarters);
 }
 
-console.log(distanceFromHqInBlocks(50));
-console.log(distanceFromHqInBlocks(63));
+console.log(distanceFromHqInBlocks(30));
+console.log(distanceFromHqInBlocks(37));
 
 function distanceFromHqInFeet(pickUpLocation){
 let blocks = distanceFromHqInBlocks(pickUpLocation);
   return blocks * 264;
 }
 
-console.log(distanceFromHqInFeet(50));
-console.log(distanceFromHqInFeet(67));
+console.log(distanceFromHqInFeet(22));
+console.log(distanceFromHqInFeet(17));
 
 
 function distanceTravelledInFeet(start, destination){
@@ -21,5 +21,24 @@ function distanceTravelledInFeet(start, destination){
 }
 
 console.log(distanceTravelledInFeet(34, 38)); 
-console.log(distanceTravelledInFeet(50, 60)); 
-console.log(distanceTravelledInFeet(60, 50)); 
+console.log(distanceTravelledInFeet(20, 30)); 
+console.log(distanceTravelledInFeet(10, 27)); 
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination); 
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; 
+    } else {
+        return "cannot travel that far"; 
+    }
+}
+
+console.log(calculatesFarePrice(34, 32)); 
+console.log(calculatesFarePrice(34, 40)); 
+console.log(calculatesFarePrice(34, 60)); 
+console.log(calculatesFarePrice(34, 80)); 
